a007e3089b4115e9330e2576def1cc1f26ecdf22
lxchurbakov <lxchurbakov@gmail.com>
2023-10-10 18:18:57 +0300

    Add TextArea

--- minikit/inputs.tsx
+++ minikit/inputs.tsx
@@ -42,10 +42,38 @@ export type TextInputProps = {
     placeholder?: string;
 };
 
-export const TextInput = ({ type, placeholder,  value, onChange,  size, weight, color, background, border, outline,  ...props }: TextInputProps & StyledInputProps & Input<string> & BaseProps) => {
+export const StringInput = ({ type, placeholder,  value, onChange,  size, weight, color, background, border, outline,  ...props }: TextInputProps & StyledInputProps & Input<string> & BaseProps) => {
     return (
         <Base w="100%" {...props}>
             <StyledInput placeholder={placeholder} type={type} value={value || ''} onChange={(e) => onChange(e.target.value || '')} {...{ size, weight, color, background, border, outline }} />
         </Base>
     );
+};
+
+const StyledTextArea = styled.textarea<any>`
+    font-family: ${theme.fontFamily};
+    font-size: ${props => props.size || '16px'};
+    font-weight: ${props => props.weight || 400};
+
+    color: ${props => props.color || theme.colors.text};
+    box-sizing: border-box;
+    width: 100%;
+    display: block;
+
+    border-radius: 4px;
+    border: ${props => props.border || 'none'};
+    padding: 8px 12px;
+    background: ${props => props.background || 'transparent'};
+
+    outline: ${props => props.outline || 'none'};
+    max-width: 100%;
+    min-height: 80px;
+`;
+
+export const TextInput = ({ placeholder, value, onChange, size, weight, color, background, border, outline, ...props }) => {
+    return (
+        <Base w="100%" {...props}>
+            <StyledTextArea value={value || ''} placeholder={placeholder} onInput={(e) => onChange(e.target.value || '')} {...{ size, weight, color, background, border, outline }}/>
+        </Base>
+    );  
 };
