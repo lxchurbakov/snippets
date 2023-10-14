432bc409259903a54130727d4e8ae59c45f7b350
lxchurbakov <lxchurbakov@gmail.com>
2023-10-14 10:26:40 +0300

    Add table component

--- react-components/index.tsx
+++ react-components/index.tsx
@@ -106,3 +106,74 @@ export const Logo = ({ onClick }: { onClick?: () => void }) => {
     );
 };
 
+// table.tsx
+
+import React from 'react';
+import styled from 'styled-components';
+
+import { Text } from '/src/libs/atoms';
+import { colors } from '/src/libs/theme';
+
+const TableWrap = styled.table`
+    width: 100%;
+    padding: 0;
+    border: 0;
+    border-collapse: collapse;
+`;
+
+const TableHead = styled.thead`
+    width: 100%;
+`;
+
+const TableHeadRow = styled.tr`
+    width: 100%;
+`;
+
+const TableHeadItem = styled.td`
+    width: 100%;
+    padding: 0;
+    border: 0;
+`;
+
+const TableBody = styled.tbody`
+    width: 100%;    
+`;
+
+const TableBodyRow = styled.tr`
+    width: 100%;
+`;
+
+const TableBodyItem = styled.td`
+    width: 100%;
+    padding: 0;
+    border: 0;
+    padding-bottom: 4px;
+`;
+
+export const Table = ({ columns, rows }) => {
+    return rows.length > 0 ? (
+        <TableWrap cellpadding="0" cellspacing="0">
+            <TableHead>
+                <TableHeadRow>
+                    {columns.map((column) => (
+                        <TableHeadItem key={column.key}>
+                            <Text color={colors.grey} size="13px" weight="800">{column.title}</Text>
+                        </TableHeadItem>
+                    ))}
+                </TableHeadRow>
+            </TableHead>
+
+            <TableBody>
+                {rows.map((row) => (
+                    <TableBodyRow key={row.id}>
+                        {columns.map((column) => (
+                            <TableBodyItem key={column.key}>
+                                <Text size="14px">{column.render ? column.render(row) : row[column.key]}</Text>
+                            </TableBodyItem>
+                        ))}
+                    </TableBodyRow>
+                ))}
+            </TableBody>
+        </TableWrap>
+    ) : null;
+};
